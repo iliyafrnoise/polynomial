32296116143387f37d6def3c15eabd3d8bf8017c
iliyafrnoise <iliyafro12@gmail.com>
2024-01-28 12:19:49 +0330

    ezafe kardan def  multiplication va gozashtan comment

--- project.py
+++ project.py
@@ -1,3 +1,7 @@
+#  1 دریافت چند جمله ای
+#در ابتدا ما به عنوان ورودی تعداد توان رو از کاربر دریافت میکنیم
+#و بعد با استفاده از حلقه ها تا زمانی که متعیر کمکی ما کوچکتر و مساوی تعداد توان هاست 
+#ضریب هارو دریافت میکنیم و در مجموعه ضرایب ان را اضافه میکنیم
 number_of_power = int(input("enter a number of power : "))
 i = 0
 coefficients = []
@@ -5,6 +9,13 @@ while i <= number_of_power :
     coefficient = int(input(f"enter coefficient of x^^{i} : "))
     coefficients.append(coefficient)
     i += 1
+# 1 نمایش چند جمله ای
+#یک استرینگ خالی بتعریف میکنیم 
+# با استفاده از حلقه تا زمانی که متغیر کمکی ما از تعداد توان کوچکتر است 
+# اگر درجه برابر متغیر کمکی نبود
+#  با + جمع کنه و در همون استرینگ مقدار خالی رو بریزه  x^{i}  ام در i استرینگ خالی رو با ضریب 
+#جمع کنه و در همون استرینگ مقدار رو بریزه x^{i}  ام در i در غیر اینصورت استرینگ خالی رو با ضریب ت 
+#در اخر هم استرینگ تعریف شده رو چاپ میکینم 
 equation_str = "" 
 i = 0 
 while i <= number_of_power :
@@ -14,6 +25,10 @@ while i <= number_of_power :
         equation_str += f"{coefficients[i]}x^{i}"
     i += 1
 print(equation_str)
+#دریافت چند جمله ای 2
+#در ابتدا ما به عنوان ورودی تعداد توان رو از کاربر دریافت میکنیم
+#و بعد با استفاده از حلقه ها تا زمانی که متعیر کمکی ما کوچکتر و مساوی تعداد توان هاست 
+#ضریب هارو دریافت میکنیم و در مجموعه ضرایب ان را اضافه میکنیم
 number_of_power2 = int(input("enter a number of power : "))
 i = 0
 coefficients2 = []
@@ -21,6 +36,13 @@ while i <= number_of_power2 :
     coefficient2 = int(input(f"enter coefficient of x^^{i} : "))
     coefficients2.append(coefficient2)
     i += 1
+# 2 نمایش چند جمله ای 
+#یک استرینگ خالی بتعریف میکنیم 
+# با استفاده از حلقه تا زمانی که متغیر کمکی ما از تعداد توان کوچکتر است 
+# اگر درجه برابر متغیر کمکی نبود
+#  با + جمع کنه و در همون استرینگ مقدار خالی رو بریزه  x^{i}  ام در i استرینگ خالی رو با ضریب 
+#جمع کنه و در همون استرینگ مقدار رو بریزه x^{i}  ام در i در غیر اینصورت استرینگ خالی رو با ضریب ت 
+#در اخر هم استرینگ تعریف شده رو چاپ میکینم 
 equation_str2 = ""
 i = 0 
 while i <= number_of_power2 :
@@ -31,6 +53,14 @@ while i <= number_of_power2 :
     i += 1
 print(equation_str2)
 # x{0}محاسبه مقدار تابع در 
+#این تابع مقدار تابع را در یک نقطه دلخواه محاسبه میکند
+#ابتدا تابع عدد که میخواهیم  مقدار ان را حساب کنیم میگیرد 
+#answer = 0 قرار میدهیم 
+# i= 0 میدهیم
+#است  i<=number_of_power تا زمانی که  while با اسفاده از  
+#میریزیم answer  میکنیم و در  coefficients[i]*(x**i)را به علاوه answer 
+# یکی اضافه میکنیم  i به 
+#در اخر هم مقدار تابع رو در اون نقطه چاپ میکند
 def calculate(coefficients , number_of_power ):
     x = int(input("enter x : "))
     answer = 0 
@@ -68,11 +98,12 @@ sum_result = sum(coefficients, coefficients2)
 to_be_printed = ""
 i = 0
 while i < len(sum_result):
-    to_be_printed += f"{sum_result[i]}x{i} + "
+    if i != len(sum_result):
+         to_be_printed += f"{sum_result[i]}x{i} + "
+    else :
+        to_be_printed += f"{sum_result[i]}x{i}  "
     i += 1
-
-to_be_printed += "0"
-
+print("the sum is :    ")
 print(to_be_printed)
 
 #محاسبه تفریق دو چند جمله ای
@@ -101,16 +132,73 @@ def subtraction(coefficients, coefficients2):
 
     return result
 
-sum_result = subtraction(coefficients, coefficients2)
+subtraction_result = subtraction(coefficients, coefficients2)
 to_be_printed = ""
 i = 0
-while i < len(sum_result):
-    to_be_printed += f"{sum_result[i]}x{i} + "
+while i < len(subtraction_result):
+    if i != len(subtraction_result):
+         to_be_printed += f"{subtraction_result[i]}x{i} + "
+    else :
+        to_be_printed += f"{subtraction_result[i]}x{i}  "
     i += 1
-
-to_be_printed += "0"
-
+print("the subtraction is :    ")
 print(to_be_printed)
+
+#ضرب دو چند جمله ای
+def Multiplication(coefficients,coefficients2 , number_of_power , number_of_power2):
+    multiplication= []
+    max_degree = number_of_power+number_of_power2 + 1
+    i = 0
+    while i <= max_degree :
+        multiplication.append(0)
+        i += 1
+    for i in range(0,number_of_power+1) :
+        for j in range(0,number_of_power2+1):
+            multiplication[i+j] += coefficients[i]*coefficients2[j]
+    return multiplication
+multi_poly = Multiplication(coefficients,coefficients2 , number_of_power , number_of_power2)
+to_be_printed = ""
+i = 0
+while i < len(multi_poly):
+    if i != len(multi_poly):
+         to_be_printed += f"{multi_poly[i]}x{i} + "
+    else :
+        to_be_printed += f"{multi_poly[i]}x{i}  "
+    i += 1
+print("the multiplication is :    ")
+print(to_be_printed)   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 #numpyجمع با 
@@ -160,7 +248,6 @@ print(Multiplication_string)
 # numpyمشتق با  
 
 import numpy as np
-coefficients.reverse()
 derivative_coefficients = np.polyder(coefficients)
 derivative_string = np.poly1d(derivative_coefficients)
 print("Derivative of the resulting polynomial with numpy:")
@@ -172,6 +259,9 @@ integral_coefficients = np.polyint(coefficients)
 integral_string = np.poly1d(integral_coefficients)
 print("integral of the resulting polynomial with numpy")
 print(integral_string)
+#numpy ریشه با 
+
+
 
 
 
